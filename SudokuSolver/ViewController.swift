8eca96ac4eb3ed6d06bd3d4cf1038abbeb536f0c
Takanari Hayama <taki@igel.co.jp>
2015-10-13 17:30:00 +0900

    Initial Commit

---
+++ SudokuSolver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SudokuSolver
+//
+//  Created by Takanari Hayama on 10/13/15.
+//  Copyright © 2015 IGEL Co.,Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
